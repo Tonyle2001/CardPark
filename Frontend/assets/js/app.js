749efa5366f163ed9e85cfc1a8ef1420979fd0b0
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-02-28 22:50:18 -0600

    Update app.js

---
+++ Frontend/assets/js/app.js
@@ -0,0 +1,29 @@
+class App {
+
+  constructor(view, userModel) {
+  
+    this.view = view;
+    
+    this.userModel = userModel;
+  }
+
+  login() {
+  
+    const { email, password } = this.view.getCredentials();
+    
+    if (email !== this.userModel.user.email) {
+    
+      this.view.displayMessage('Incorrect email.', false);
+      
+    } else if (password !== this.userModel.user.password) {
+    
+      this.view.displayMessage('Incorrect password.', false);
+      
+    } else {
+    
+      this.view.displayMessage('Login Successful!', true);
+      
+      window.location.href = 'Home.html'; // Redirect to home page on successful login
+    }
+  }
+}
