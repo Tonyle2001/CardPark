2c1234a77677123b64de670360b1a81e1560e40a
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-03-12 11:47:38 -0500

    Update profileApp.js

--- Frontend/assets/js/profileApp.js
+++ Frontend/assets/js/profileApp.js
@@ -1,1 +1,29 @@
+// profileApp.js
+var profileApp = {
+  
+    init: function() {
+      
+        profileListeners.attachSubmitListener(profileModel, profileView);
+    }
+};
+
+profileApp.init();
+
+// sideNavApp part below
+
+var sideNavApp = (function() {
+  
+    function toggleNav() {
+      
+        var currentState = sideNavModel.getSidebarState();
+      
+        sideNavModel.setSidebarState(!currentState);
+      
+        sideNavView.updateSidebar();
+    }
+
+    return {
+        toggleNav: toggleNav
+    };
+})();
 
