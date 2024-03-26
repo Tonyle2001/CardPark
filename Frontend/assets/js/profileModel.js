0c1d23b649ff9631ccd35460c80633c9c8dc9d1e
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-03-26 10:07:35 -0500

    Update profileModel.js

--- Frontend/assets/js/profileModel.js
+++ Frontend/assets/js/profileModel.js
@@ -1,26 +1,25 @@
 // profileModel.js
 var profileModel = {
-  
+
     carMake: '',
-  
+    
     carModel: '',
-  
+    
     carColor: '',
-  
+    
     carYear: '',
-  
+    
     carLicensePlate: '',
 
     updateCarInfo: function(make, model, color, year, licensePlate) {
-      
         this.carMake = make;
-      
+        
         this.carModel = model;
-      
+        
         this.carColor = color;
-      
+        
         this.carYear = year;
-      
+        
         this.carLicensePlate = licensePlate;
     }
 };
@@ -28,23 +27,18 @@ var profileModel = {
 // sideNavModel part below
 
 var sideNavModel = (function() {
-  
     var sidebarState = false; // false means closed, true means open
 
     function setSidebarState(state) {
-      
         sidebarState = state;
     }
 
     function getSidebarState() {
-      
         return sidebarState;
     }
 
     return {
-      
         setSidebarState: setSidebarState,
-      
         getSidebarState: getSidebarState
     };
 })();
