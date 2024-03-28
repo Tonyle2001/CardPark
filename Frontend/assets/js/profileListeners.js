7ed3958845576e96e4d17693cc8f801dfd87db66
Thomas Van Fleet <tlvanfleet@noctrl.edu>
2024-03-28 11:14:45 -0500

    Fixed hrefs

--- Frontend/assets/js/profileListeners.js
+++ Frontend/assets/js/profileListeners.js
@@ -1,24 +1,24 @@
 // profileListeners.js
 var profileListeners = {
-
+  
     attachSubmitListener: function(model, view) {
-    
+      
         var submitButton = document.getElementById('vehicleForm').querySelector('button');
-        
+      
         submitButton.onclick = function() {
-        
+          
             var make = document.getElementById('carMake').value;
-            
+          
             var modelValue = document.getElementById('carModel').value;
-            
+          
             var color = document.getElementById('carColor').value;
-            
+          
             var year = document.getElementById('carYear').value;
-            
+          
             var licensePlate = document.getElementById('carLicensePlate').value;
             
             model.updateCarInfo(make, modelValue, color, year, licensePlate);
-            
+          
             view.displayVehicleInfo(model);
         };
     }
@@ -27,17 +27,20 @@ var profileListeners = {
 // sideNavListeners part below
 
 var sideNavListeners = (function() {
+  
     function init() {
-    
+      
         document.querySelector('.openbtn').addEventListener('click', sideNavApp.toggleNav);
-        
+      
         document.querySelector('.closebtn').addEventListener('click', sideNavApp.toggleNav);
     }
 
     if (document.readyState === 'loading') {
-    
+      
         document.addEventListener('DOMContentLoaded', init);
+      
     } else {
+      
         init(); // DOMContentLoaded has already fired
     }
 
