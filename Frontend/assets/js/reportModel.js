99763ab8cb0476106a13c8aeb15bf95f3678eda7
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-01 13:24:49 -0500

    Update reportModel.js

--- Frontend/assets/js/reportModel.js
+++ Frontend/assets/js/reportModel.js
@@ -1,1 +1,35 @@
+// reportModel.js
+class Model {
+    constructor() {
+        this.issue = null;
+    }
 
+    setIssue(issue) {
+        this.issue = issue;
+    }
+
+    getIssue() {
+        return this.issue;
+    }
+}
+
+
+
+// sideNavModel part below
+
+var sideNavModel = (function() {
+    var sidebarState = false; // false means closed, true means open
+
+    function setSidebarState(state) {
+        sidebarState = state;
+    }
+
+    function getSidebarState() {
+        return sidebarState;
+    }
+
+    return {
+        setSidebarState: setSidebarState,
+        getSidebarState: getSidebarState
+    };
+})();
