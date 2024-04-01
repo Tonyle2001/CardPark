6367182bdfa757c8b11c05d3ab1e914ff95f0c4c
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-01 13:28:17 -0500

    Update reportApp.js

--- Frontend/assets/js/reportApp.js
+++ Frontend/assets/js/reportApp.js
@@ -1,1 +1,47 @@
+// reportApp.js
+document.addEventListener('DOMContentLoaded', () => {
+
+    const model = new Model();
+    
+    const view = new View();
+    
+    const controller = new Controller(model, view);
+    
+    setupListeners(view, controller);
+});
+
+// Part of reportApp.js
+
+class Controller {
+
+    constructor(model, view) {
+    
+        this.model = model;
+        
+        this.view = view;
+    }
+
+    handleIssueSelection(issue) {
+    
+        this.model.setIssue(issue);
+        
+        this.view.navigateToIssuePage(issue);
+    }
+}
+// sideNavApp part below
+
+var sideNavApp = (function() {
+    function toggleNav() {
+    
+        var currentState = sideNavModel.getSidebarState();
+        
+        sideNavModel.setSidebarState(!currentState);
+        
+        sideNavView.updateSidebar();
+    }
+
+    return {
+        toggleNav: toggleNav
+    };
+})();
 
