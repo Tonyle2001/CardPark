4fe0f38a85cdba4137dfe1d0bad6ef5df33dbb36
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-01 13:25:41 -0500

    Update reportListeners.js

--- Frontend/assets/js/reportListeners.js
+++ Frontend/assets/js/reportListeners.js
@@ -1,1 +1,31 @@
+// reportListeners.js
+function setupListeners(view, controller) {
 
+    view.submitButton.addEventListener('click', () => {
+    
+        const issue = view.getSelectedIssue();
+        
+        controller.handleIssueSelection(issue);
+    });
+}
+
+
+
+// sideNavListeners part below
+
+var sideNavListeners = (function() {
+    function init() {
+        document.querySelector('.openbtn').addEventListener('click', sideNavApp.toggleNav);
+        document.querySelector('.closebtn').addEventListener('click', sideNavApp.toggleNav);
+    }
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', init);
+    } else {
+        init(); // DOMContentLoaded has already fired
+    }
+
+    return {
+        init: init
+    };
+})();
