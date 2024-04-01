49a3b42357c73cd753b0e731ed495830b3f84bfe
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-01 13:26:55 -0500

    Update reportView.js

--- Frontend/assets/js/reportView.js
+++ Frontend/assets/js/reportView.js
@@ -1,1 +1,52 @@
+// reportView.js
+class View {
+    constructor() {
+        this.issueSelector = document.getElementById('issue-selector');
+        
+        this.submitButton = document.getElementById('submit-button');
+    }
 
+    getSelectedIssue() {
+        return this.issueSelector.value;
+    }
+
+    navigateToIssuePage(issue) {
+    
+        const issuePageMap = {
+            'parked-car': 'reportParkedCar.html',
+            
+            'hazard-item': 'reportHazardItem.html',
+            
+            'weather-issue': 'reportWeatherIssue.html',
+        };
+
+        const targetPage = issuePageMap[issue];
+        
+        if (targetPage) {
+        
+            window.location.href = targetPage;
+        } else {
+        
+            alert('Please select an issue.');
+        }
+    }
+}
+
+
+
+// sideNavView part below
+
+var sideNavView = (function() {
+    function updateSidebar() {
+    
+        var isOpen = sideNavModel.getSidebarState();
+        
+        document.getElementById("mySidenav").style.width = isOpen ? "250px" : "0";
+        
+        document.getElementById("main").style.marginLeft = isOpen ? "250px" : "0";
+    }
+
+    return {
+        updateSidebar: updateSidebar
+    };
+})();
