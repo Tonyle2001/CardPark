82edaae941b689044b168caa51be4ab5f0899e53
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-04 10:41:07 -0500

    Update reportListeners.js

--- Frontend/assets/js/reportListeners.js
+++ Frontend/assets/js/reportListeners.js
@@ -1,15 +1,123 @@
 // reportListeners.js
+
+document.getElementById('parkingLot-selector').addEventListener('change', function() {
+    updateSpotNumberValidation(this.value);
+});
+
+function updateSpotNumberValidation(selectedLot) {
+    var spotNumberInput = document.getElementById('spotNumber');
+
+    // Remove previous event listeners to avoid stacking them
+    spotNumberInput.removeEventListener('input', validateSpotNumber);
+
+    switch(selectedLot) {
+        case 'A':
+            spotNumberInput.dataset.allowedMax = 137;
+            break;
+            
+        case 'B':
+        
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'C':
+            
+            spotNumberInput.dataset.allowedMax = 124; 
+            break;
+            
+        case 'E':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'F':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'G':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'H':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'I':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'J':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'L':
+            
+            spotNumberInput.dataset.allowedMax = 200; 
+            break;
+            
+        case 'M':
+            
+            spotNumberInput.dataset.allowedMax = '200'; 
+            break;
+            
+        case 'O':
+            
+            spotNumberInput.dataset.allowedMax = '200'; 
+            break;
+            
+        case 'P':
+            
+            spotNumberInput.dataset.allowedMax = '200'; 
+            break;
+            
+        default:
+            spotNumberInput.dataset.allowedMax = '1'; // Default or max value for other lots
+            break;
+    }
+
+    spotNumberInput.addEventListener('input', validateSpotNumber);
+}
+
+function validateSpotNumber() {
+
+    var max = this.dataset.allowedMax;
+    
+    var value = parseInt(this.value, 10);
+
+    if ((isNaN(value) || value < 0 || value > max) && this.value !== '') {
+    
+        this.style.borderColor = 'red'; // Indicate error
+        
+        alert('Please enter a valid spot number.');
+        
+    } else {
+    
+        this.style.borderColor = 'green'; // Reset to default or valid color
+        
+        this.setCustomValidity('');
+        
+        
+    }
+}
+
 function setupListeners(view, controller) {
 
     view.submitButton.addEventListener('click', () => {
     
         const issue = view.getSelectedIssue();
         
+        const lot = view.getSelectedLot();
+        
         controller.handleIssueSelection(issue);
+        
+        controller.handleLotSelection(lot);
     });
 }
-
-
 
 // sideNavListeners part below
 
