b517aab9cc1167d82fd69491dba695abdf681be4
Alberto Maldonado <mr-alberto@outlook.com>
2024-04-16 10:53:55 -0500

    Cookies and Folder Restructure

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+router.get('/',function(req, res){
+  res.send('Hello from Index');
 });
+
 
 module.exports = router;
