1d2ef4c25d5b9ef54652ad569e1d443e869774ed
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-16 10:25:32 -0500

    Create parkingLotApp.js

---
+++ Frontend/assets/js/parkingLotApp.js
@@ -0,0 +1,16 @@
+// sideNavApp part below
+
+var sideNavApp = (function() {
+    function toggleNav() {
+    
+        var currentState = sideNavModel.getSidebarState();
+        
+        sideNavModel.setSidebarState(!currentState);
+        
+        sideNavView.updateSidebar();
+    }
+
+    return {
+        toggleNav: toggleNav
+    };
+})();
