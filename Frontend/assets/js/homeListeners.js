bbdd3eda145c06c13d59a6af6c4ed4eb74c7181d
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-16 10:35:36 -0500

    Update homeListeners.js

--- Frontend/assets/js/homeListeners.js
+++ Frontend/assets/js/homeListeners.js
@@ -1,4 +1,30 @@
+
 function onClick(clicked_id){
 	sessionStorage.setItem("clicked", clicked_id);
-	
+	location.href="../LoginPage/parkingLot.html";
 }
+
+var HomeListeners = (function() {
+
+    function init() {
+    
+        document.querySelector('.openbtn').addEventListener('click', HomeApp.toggleNav);
+        
+        document.querySelector('.closebtn').addEventListener('click', HomeApp.toggleNav);
+    }
+
+    // Ensure listeners are initialized when the DOM is fully loaded
+    if (document.readyState === 'loading') {
+    
+        document.addEventListener('DOMContentLoaded', init);
+        
+    } else {
+    
+        init();  // DOMContentLoaded has already fired
+    }
+
+    return {
+    
+        init: init
+    };
+})();
