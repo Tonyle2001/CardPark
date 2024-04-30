4b9f129b4fc62d5283f0ea7c70d2845bfd4c8ae6
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2024-04-29 23:06:05 -0500

    Update lotAListeners.js

--- Frontend/assets/js/lotAListeners.js
+++ Frontend/assets/js/lotAListeners.js
@@ -1,4 +1,3 @@
-
 function setupListeners(view, model) {
 
     document.querySelectorAll('.parking-spot').forEach(function(button, index) {
@@ -7,35 +6,26 @@ function setupListeners(view, model) {
         
             if (!model.spots[index].occupied) {
             
-                // Attempt to reserve the spot
-                
-                model.setOccupied(index, true);
+                model.setOccupied(index);
                 
                 view.updateButton(index, true);
-            } else {
-            
-                // Display message if the spot is already occupied
-                alert('This spot is taken and cannot be reserved.');
             }
         });
     });
-
-    // Additional listener setup, if any
 }
-
-
 const wrapper = document.getElementById('parking-lot');
 
-// Event listener for all buttons within the parking lot
+//Event listener for all buttons within the parking lot
 wrapper.addEventListener('click', (event) => {
-
-    const isButton = event.target.nodeName === 'BUTTON';
-    
-    if (!isButton) return;
-    
-    var ident = event.target.getAttribute('id');
-    
-    window.parent.postMessage(ident, '*'); // Send message to parent window if needed
+	
+	//Check that what is being clicked is a button, so clicking the div doesn't trigger the dialog.
+	const isButton = event.target.nodeName === 'BUTTON';
+	if(!isButton)
+		return;
+	
+	var ident = event.target.getAttribute('id');
+	
+	window.parent.postMessage(ident, '*');
+	
 });
-
 
